67683065f2819459035b92817b928c6f5bbd3afc
greeksharifa <greeksharifa.gmail.com>
2018-08-15 17:42:44 +0900

    track dummy1, untrack dummy2
    This commit is written in git07: git commit -v

--- first.py
+++ first.py
@@ -1,5 +1,11 @@
 print("Hello, git!") # instead of "Hello, World!"
 print("Hi, git!!")
 
+print('1st')
+
 print("This is the 1st sentence written in 1st-branch.")
 print("This is the 1st sentence written in 3rd-branch.")
+
+print('2nd')
+
+print('test git add .')
