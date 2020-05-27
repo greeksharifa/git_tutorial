96c99dc2ded5ed4cd9ca6881b80e99880049b96d
greeksharifa <greeksharifa@gmail.com>
2020-05-27 20:28:34 +0900

    fourth.py

--- first.py
+++ first.py
@@ -1,13 +1,1 @@
-print("Hello, git!") # instead of "Hello, World!"
-print("Hi, git!!")
-
-print('1st')
-
-print("This is the 1st sentence written in 1st-branch.")
-print("This is the 1st sentence written in 3rd-branch.")
-
-print('2nd')
-
-print('test git add .')
-
-print("Desired sentence in 2nd-branch")
+print("Hello, git!") 
