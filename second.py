8506cef225d4b0b5d9c657afdcfde40c8c04a238
greeksharifa <greeksharifa@gmail.com>
2020-05-27 21:42:19 +0900

    wow

--- second.py
+++
@@ -1,9 +0,0 @@
-print('1st')
-print("Why don't you answer me, git?")
-
-print('2nd')
-print("This is the 1st sentence written in 2nd-branch.")
-
-print('3rd')
-print('4th')
-print('test git add -u')
